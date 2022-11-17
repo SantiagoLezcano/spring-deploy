79a937c003bb4aa78e49a5e14949271644b77ce4
santiagolezcano <santiago.lezcano99@gmail.com>
2022-11-17 15:27:28 -0500

    JUEGO

--- src/main/java/com/example/springdeploy/HelloController.java
+++ src/main/java/com/example/springdeploy/HelloController.java
@@ -30,7 +30,7 @@ public class HelloController {
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
                   </head>
                   <body>
-                    <h1>Hola mundo desde spring boot!</h1>
+                    <h1>CAROLINA TE AMO MUCHO!</h1>
                     <a class="btn btn-primary" href="https://www.youtube.com/watch?v=6kq1_J-R6JE">PRESIONA<a/>
                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
                   </body>
